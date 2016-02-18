4e467ee783d6b10a8753c851651f30402c73eeae
ChrisMurphy <monkeeman69@googlemail.com>
2016-02-18 22:55:39 +0000

    Fix lint error, unused import

--- src/app/features/app/app.ts
+++ src/app/features/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, View } from 'angular2/core';
+import { Component } from 'angular2/core';
 import { ROUTER_DIRECTIVES, RouteConfig } from 'angular2/router';
 
 import { HomeComponent } from '../home/home';
